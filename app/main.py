240eb2905b9575f75a7d08a011221f37b352f31a
lickelon <jindoman0@naver.com>
2026-01-29 21:59:50 +0900

    Add week3 sample code

--- app/main.py
+++ app/main.py
@@ -1,21 +1,11 @@
 from fastapi import FastAPI
 
-from app.week2.practice1 import router as practice1_router
-from app.week2.practice2 import router as practice2_router
-from app.week2.practice3 import router as practice3_router
+from app.week3.items_router import items_router
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
-@app.post("/items/{item}")
-async def create_item(item: str):
-    return {"item": item}
-
-app.include_router(practice1_router)
-app.include_router(practice2_router)
-app.include_router(practice3_router)
+app.include_router(items_router)
